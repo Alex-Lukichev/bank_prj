15203b74f7a1b5cfbd0cd75933f5d64e1e32e45e
Alexander Lukichev <allukichev@gmail.com>
2024-09-06 11:08:06 +0400

    functions mask_account_card and get_date modified

--- src/widget.py
+++ src/widget.py
@@ -1,28 +1,35 @@
-from masks import get_mask_card_number
-from masks import get_mask_account
+from datetime import datetime
 
-def mask_account_card(card_or_acc_number: str) -> str:
+from masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(card_input: str) -> str:
     """Функция, возвращающая замаскированный номер карты или счета"""
-    # masked_number = str()
-    number_list = card_or_acc_number.split()
-
-    if number_list[0] in ['Maestro', 'MasterCard', 'Visa']:
-        number_list[-1] = get_mask_card_number(number_list[-1])
-    elif number_list[0] in ['Счет']:
-        number_list[-1] = get_mask_account(number_list[-1])
+    masked_number = str()
+    # number_list = card_or_acc_number.split()
+    if "Maestro" in card_input or "MasterCard" in card_input or "Visa" in card_input:
+        masked_number = card_input[:-16] + get_mask_card_number(card_input[-16:])
+    elif "Счет" in card_input:
+        masked_number = card_input[:-20] + get_mask_account(card_input[-20:])
     else:
         print('Введите корректное обозначение типа карты или Счет')
-    return ' '.join(number_list)
+
+    # if number_list[0] in ['Maestro', 'MasterCard', 'Visa']:
+    #     number_list[-1] = get_mask_card_number(number_list[-1])
+    # elif number_list[0] in ['Счет']:
+    #     number_list[-1] = get_mask_account(number_list[-1])
+    # else:
+    #     print('Введите корректное обозначение типа карты или Счет')
+    return masked_number
 
 
 def get_date(date_time: str) -> str:
     """Функция возвращает дату в формате ДД.ММ.ГГГГ"""
-    _year = date_time[0:4]
-    _month = date_time[5:7]
-    _day = date_time[8:10]
-    return '.'.join([_day, _month, _year])
+    decoded_datetime = datetime.strptime(date_time, "%Y-%m-%dT%H:%M:%S.%f")
+    converted_date = decoded_datetime.strftime("%d.%m.%Y")
+    return converted_date
 
 
 if __name__ == '__main__':
-    # print(mask_account_card(input("Insert your card or account: ")))
+    print(mask_account_card(input("Insert your card or account: ")))
     print(get_date(input("Insert date and time: ")))
